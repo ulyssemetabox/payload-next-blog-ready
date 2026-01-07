1a62a94d747a11735e7e74d1c8754005350166ed
ulyssemetabox <ulysse.metabox@outlook.com>
2026-01-07 17:18:41 +0400

    removed autologin

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -21,10 +21,6 @@ export default buildConfig({
         importMap: {
             baseDir: path.resolve(dirname),
         },
-        autoLogin: {
-            email: env.CMS_SEED_ADMIN_EMAIL,
-            password: env.CMS_SEED_ADMIN_PASSWORD,
-        },
     },
     collections: [Users, Media, Articles, ArticleAuthors],
     editor: lexicalEditor({
