546db9e8ce94ec098ae641b2bb6e571d87c336a6
ulyssemetabox <ulysse.metabox@outlook.com>
2026-01-08 10:46:49 +0400

    added migration file

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260108_064230 from './20260108_064230';
+
+export const migrations = [
+  {
+    up: migration_20260108_064230.up,
+    down: migration_20260108_064230.down,
+    name: '20260108_064230'
+  },
+];
